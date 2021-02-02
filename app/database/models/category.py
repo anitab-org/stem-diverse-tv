461e43ec692e75683291f8f6c620241b5523950b
codewithjalaj <jalaj@mail.com>
2021-02-02 15:23:33 +0530

    feat: Add GET all categories endpoint

--- app/database/models/category.py
+++ app/database/models/category.py
@@ -9,18 +9,21 @@ class CategoryModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text())
-    section = db.relationship('SectionModel', secondary=category_section,
-                              backref=db.backref('category', lazy='dynamic'))
+    section = db.relationship(
+        "SectionModel",
+        secondary=category_section,
+        backref=db.backref("category", lazy="dynamic"),
+    )
 
     def __init__(self, title):
         self.title = title
-    
+
     @classmethod
-    def find_by_title(cls, title: str) -> 'CategoryModel':
+    def find_by_title(cls, title: str) -> "CategoryModel":
         return cls.query.filter_by(title=title.capitalize()).first()
-    
+
     def save_to_db(self) -> None:
-        '''Add category to database'''
+        """Add category to database"""
         db.session.add(self)
         db.session.commit()
 
@@ -30,6 +33,9 @@ class CategoryModel(db.Model):
         db.session.commit()
 
     def delete_from_db(self) -> None:
-        '''Deletes category from the database.'''
+        """Deletes category from the database."""
         db.session.delete(self)
         db.session.commit()
+
+    def json(self):
+        return {"id": self.id, "title": self.title}

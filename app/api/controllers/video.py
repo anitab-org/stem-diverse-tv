b35fc0665adc0f1681218920f1b1f59f3420d993
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2021-02-04 00:59:03 +0530

    Removed authentication token requirement from GET /video/latest (#71)
    * removed @token_required from GET /video/latest
    
    * removed rest of authorization

--- app/api/controllers/video.py
+++ app/api/controllers/video.py
@@ -19,12 +19,6 @@ add_models_to_namespace(video_ns)
 
 @video_ns.route("/latest")
 class VideoLibrary(Resource):
-    @token_required
-    @video_ns.doc(
-        params={
-            "authorization": {"in": "header", "description": "An authorization token"}
-        }
-    )
     def get(self):
         with open("./content/latests.json") as f:
             return json.loads(f.read())

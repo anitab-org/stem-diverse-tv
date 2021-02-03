6a14889cd96c581d4b0354ae511c72e658ed1453
Jalaj <Jalaj@mail.com>
2021-02-03 23:53:03 +0530

    feat: Add YouTube video POST endpoint (#58)
    * feat: Create add youtube video POST endpoint
    
    * refactor: Refactor add youtube video endpoint code
    
    Moved the models in different file, created another file for extracting id, added check when  no video is found
    
    #22

--- app/api/controllers/video.py
+++ app/api/controllers/video.py
@@ -1,9 +1,9 @@
 from flask import request, Response, jsonify
+import os
 import json
-from flask_restplus import Api, Resource, Namespace
-
+import requests
+from flask_restplus import Api, Resource, Namespace, reqparse, marshal
 from app.api.dao.section_dao import SectionDAO
-
 from app.api.models.video import *
 from app.api.validations.video import validate_video_creation_data
 from app.api.dao.video_dao import VideoDAO
@@ -11,6 +11,7 @@ from app.api.dao.author_dao import AuthorDAO
 from datetime import datetime
 from ..mappers.video_mapper import map_to_dto
 from app.api.middlewares.auth import token_required
+from app.utils.extract_video_id import extract_video_id
 
 video_ns = Namespace("video", description="Video Library")
 add_models_to_namespace(video_ns)
@@ -88,6 +89,101 @@ class Video(Resource):
                 data.get("free_to_reuse"),
                 data.get("authorized_to_reuse"),
             )
+
+            VideoDAO.add_video_authors(video, authors)
+            VideoDAO.add_video_sections(video, sections)
+
+        response["video"] = map_to_dto(video)
+        return response, 200
+
+
+@video_ns.route("/youtube")
+class AddYoutubeVideo(Resource):
+    @token_required
+    @video_ns.doc(
+        params={
+            "authorization": {"in": "header", "description": "An authorization token"}
+        }
+    )
+    @video_ns.expect(add_yt_video_model)
+    def post(self):
+        payload = request.json
+        video_url = payload["url"]
+        video_id = extract_video_id(video_url)
+        response = requests.get(
+            f'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cid%2Clocalizations%2Cstatistics%2CtopicDetails&id={video_id}&key={os.environ.get("API_KEY")}'
+        )
+        video_json = response.json()
+        response = {"notes": []}
+
+        if len(video_json["items"]) == 0:
+            response["notes"].append("No video found, please check the url.")
+            return response, 404
+        else:
+            video = video_json["items"][0]
+
+        video_data = {
+            "title": video["snippet"]["title"],
+            "url": video_url,
+            "preview_url": video["snippet"]["thumbnails"]["standard"]["url"],
+            "date_published": video["snippet"]["publishedAt"].split("T")[0],
+            "source": "YouTube",
+            "channel": video["snippet"]["channelTitle"],
+            "duration": video["contentDetails"]["duration"].split("T")[1],
+            "archived": False,
+            "free_to_reuse": video["contentDetails"]["licensedContent"],
+            "authorized_to_reuse": video["contentDetails"]["licensedContent"],
+            "category_sections": payload["sections"][0]["id"],
+            "authors": list(payload["authors"]),
+        }
+
+        validation_result = validate_video_creation_data(video_data)
+
+        if validation_result is not None:
+            return validation_result
+        url = video_data["url"]
+        existing_video = VideoDAO.find_video_by_url(url)
+
+        # find authors from the list
+        authors = AuthorDAO.find_authors_by_ids(video_data["authors"])
+        if authors.count() != len(video_data["authors"]):
+            response["notes"].append(
+                "Some of the authors are not valid or they do not exist."
+            )
+
+        # find sections from the list
+        sections = SectionDAO.find_sections_by_ids(video_data["category_sections"])
+        if sections.count() != len(video_data["category_sections"]):
+            response["notes"].append(
+                "Some of the sections are not valid or they do not exist."
+            )
+
+        # remove association fields
+        video_data.pop("authors")
+        video_data.pop("category_sections")
+
+        if existing_video:
+            video_data["date_published"] = datetime.strptime(
+                video_data["date_published"], "%Y-%m-%d"
+            )
+            existing_video.update(video_data)
+            VideoDAO.replace_video_authors(existing_video, authors)
+            VideoDAO.replace_video_sections(existing_video, sections)
+            video = existing_video
+        else:
+            video = VideoDAO.create_video(
+                video_data["title"],
+                video_data["url"],
+                video_data["preview_url"],
+                datetime.strptime(video_data["date_published"], "%Y-%m-%d"),
+                video_data["source"],
+                video_data["channel"],
+                video_data["duration"],
+                video_data.get("archived"),
+                video_data.get("free_to_reuse"),
+                video_data.get("authorized_to_reuse"),
+            )
+
             VideoDAO.add_video_authors(video, authors)
             VideoDAO.add_video_sections(video, sections)
 
--- app/api/models/video.py
+++ app/api/models/video.py
@@ -3,6 +3,8 @@ from flask_restplus import Model, fields
 
 def add_models_to_namespace(api_namespace):
     api_namespace.models[add_video_model.name] = add_video_model
+    api_namespace.models[add_yt_video_model.name] = add_yt_video_model
+    api_namespace.models[yt_video_section_field.name] = yt_video_section_field
 
 
 add_video_model = Model(
@@ -30,5 +32,26 @@ add_video_model = Model(
             fields.Integer(), description="list of category sections ids"
         ),
         "authors": fields.List(fields.Integer(), description="list of authors ids"),
+    },
+)
+
+yt_video_section_field = Model(
+    "section field of add youtube video model",
+    {
+        "title": fields.String(required=True, description="section title"),
+        "category": fields.String(required=True, description="category title"),
+        "id": fields.String(required=True, description="existing section id"),
+    },
+)
+
+add_yt_video_model = Model(
+    "Fields needed for adding new Youtube Video",
+    {
+        "url": fields.String(
+            description="YouTube video url",
+            required=True,
+        ),
+        "sections": fields.List(fields.Nested(yt_video_section_field)),
+        "authors": fields.List(fields.Integer(description="existing author id")),
     },
 )

1a184ed16c84247b6f0c68f3c46977f5c366d08a
NenadPantelic <nenadpantelickg@gmail.com>
2021-02-27 15:36:38 +0100

    Add environment configuration modes and enable persistent database settings (fixes#105)

--- run.py
+++ run.py
@@ -3,11 +3,17 @@ from dotenv import load_dotenv, find_dotenv
 from firebase_admin import credentials
 from flask import Flask
 
+from app.api.controllers import api
+from app.database.sqlalchemy_extension import db
+from config import LocalConfig, get_env_config
 
-def create_app() -> Flask:
+
+def create_app(config_env=None) -> Flask:
     app = Flask(__name__)
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///local_data.db"
+    if config_env:
+        app.config.from_object(config_env)
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = LocalConfig.SQLALCHEMY_DATABASE_URI
     app.url_map.strict_slashes = False
 
     load_dotenv(find_dotenv())
@@ -16,26 +22,20 @@ def create_app() -> Flask:
     cred = credentials.Certificate("google-credentials.json")
     firebase_admin.initialize_app(cred)
 
-    from app.api.controllers import api
-
     api.init_app(app)
-
-    from app.database.sqlalchemy_extension import db
-
     db.init_app(app)
 
     return app
 
 
-application = create_app()
+config, port = get_env_config().values()
+application = create_app(config)
 
 
 @application.before_first_request
 def create_tables():
-    from app.database.sqlalchemy_extension import db
-
     db.create_all()
 
 
 if __name__ == "__main__":
-    application.run(port=5000)
+    application.run(port=port)

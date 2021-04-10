3daaea23750cefad4160d6f3698c27bc7e299c75
Anna <bauza.ania@gmail.com>
2021-04-10 13:15:02 +0100

    update base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/stem-diverse-tv/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
